a7f970e06d396500d009758dbb67582ae10c26ec
Tigran Tumasov <shugar348@gmail.com>
2017-04-01 21:03:37 +0300

    Desktop layout without responsive

--- app/initialize.js
+++ app/initialize.js
@@ -1,14 +1,46 @@
 import $ from "jquery";
 
+function hideModal (el) {
+  $(el).on('click', () => {
+    $('body').css('overflow', 'auto')
+    $('.registration')
+      .css('display', 'none')
+      .show()
+      .removeClass('registration--active')
+      .delay(300)
+      .fadeOut(0)
+
+    if (el === '.registration-form__cancel') {
+      $('.registration-form__input').map((index, item) => {
+        $(item).val('')
+      })
+    }
+  })
+}
+
+function hideModalByEsc () {
+  window.onkeydown = (event) => {
+    $('body').css('overflow', 'auto')
+    if (event.keyCode == 27) return $('.registration')
+      .css('display', 'none')
+      .show()
+      .removeClass('registration--active')
+      .delay(300)
+      .fadeOut(0)
+  }
+}
+
 $(document).ready(() => {
+  $('.main-button').on('click', () => {
+    $('body').css('overflow', 'hidden')
+    $('.registration')
+      .css('display', 'flex')
+      .hide()
+      .fadeIn(0)
+      .addClass('registration--active')
+  })
 
-  $('a[href^="#"]').on('click', function() {
-    let el = $(this).attr('href');
-    $('body').animate({scrollTop: $(el).offset().top}, 500);
-  });
-
-  $(window).on("scroll", () => {
-    let fromTop = $("body").scrollTop();
-    $('.sticky-header').toggleClass("sticky", (fromTop > 579));
-  });
+  hideModal('.registration-overlay')
+  hideModal('.registration-form__cancel')
+  hideModalByEsc()
 });
